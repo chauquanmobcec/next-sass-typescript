9c13ba5fbe8f186ba0b74c296067562e4f55e742
Minh Quan <chauquan@mobcec.com>
2022-09-20 15:28:10 +0700

    first commit

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -9,7 +9,7 @@ const HomePage = (props: Props) => {
         <section className={`jumbotron text-center ${styles.jumbotron}`} >
           <div className="container">
             <h1 className="jumbotron-heading">Album example</h1>
-            <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+            <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don&apos;t simply skip over it entirely.</p>
             <p>
               <a href="#" className="btn btn-primary m-2">Main call to action</a>
               <a href="#" className="btn btn-secondary m-2">Secondary action</a>
